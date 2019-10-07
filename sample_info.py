874eb074800d21b96be51188f292c472d908042c
Max Herz <maxopie@gmail.com>
2019-10-06 21:09:42 -0400

    updated sample file

--- sample_info.py
+++ sample_info.py
@@ -1,4 +1,8 @@
 #example of formatting for info file
+
+#email and password to send text messages from 
 email = 'person@gmail.com'
 password = 'password'
+
+#numbers and carriers to send messages to
 numbers = {'0000000000{}':'verizon', '1111111111{}':'att'}
